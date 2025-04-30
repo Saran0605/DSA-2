c1b2ae94ae41d00592ee0a05cd6111e4c332680a
Saran0605 <saransoundharajan05@gmail.com>
2025-04-30 11:28:24 +0530

    find even number of digits in array

---
+++ summa.java
@@ -0,0 +1,18 @@
+public class summa {
+    public static void main(String[] args) {
+        Float a = Float.MIN_VALUE;
+        Float b = Float.MAX_VALUE;
+        Long c = Long.MIN_VALUE;
+        Long d = Long.MAX_VALUE;
+        int e = Integer.MIN_VALUE;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+
+
+
+    }
+}

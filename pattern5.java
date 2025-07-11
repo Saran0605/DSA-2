88bb8af90502aafdd5d328131ae2bb561c8c4187
Saran0605 <saransoundharajan05@gmail.com>
2025-07-11 10:51:27 +0530

    pattern 6

--- pattern5.java
+++ pattern5.java
@@ -1,6 +1,6 @@
 public class pattern5 {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 2;
         for(int i=1;i<=2*n-1;i++){
             int colsend = i>n ? n - (i-n):i;
             int spaces = i>n?i-n:n-i;

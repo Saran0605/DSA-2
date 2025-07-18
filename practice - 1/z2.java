f516b03334a036be9aa3b8c1d8c4375cfeb70316
Saran0605 <saransoundharajan05@gmail.com>
2025-07-18 11:10:40 +0530

    min heap

---
+++ practice - 1/z2.java
@@ -0,0 +1,29 @@
+//find k th smallest element using min heap
+import java.util.*;
+public class z2 {
+    public static void main(String[] args) {
+        int[] arr = {5,7,3,9,8};
+        int k = 1;
+        int res = findkth(arr,k);
+        System.out.println(res);
+    
+        
+    }
+
+    private static int findkth(int[] arr,int k){
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for(int num:arr){
+            heap.add(num);
+        }
+
+        for(int i=1;i<k;i++){
+            heap.poll();
+        }
+        return heap.peek();
+
+        
+
+
+    }
+    
+}
